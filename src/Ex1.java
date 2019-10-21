beb5846b4a4a612139d6007b5932c1c8a7ff0921
zjxps2007 <zjxps2007@gmail.com>
2019-10-21 17:26:28 +0900

    Ex1

---
+++ src/Ex1.java
@@ -0,0 +1,12 @@
+public class Ex1 {
+    public static void main(String[] args) {
+        if(args.length > 0) {
+            for(int i =0;i<args.length;i++) {
+                System.out.print(args[i]+ " ");
+            }
+            if (args[0].equals("-h")) {
+                System.out.print("\n HELP");
+            }
+        }
+    }
+}
